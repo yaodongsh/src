ddd8f2d53c4b7f00d13fa9f73787fae60e329082
yaodong <yao_dong@126.com>
2014-12-03 10:32:56 +0800

    some other version

--- c_cpp/insert_sort.c
+++ c_cpp/insert_sort.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <assert.h>
 
+//think a[0 .. i-1] in order and put a[i] to the correct position
 void insert_sort (int a[], int n)
 {
     int j;
@@ -22,6 +24,52 @@ void insert_sort (int a[], int n)
     }
 }
 
+void insert_sort2 (int a[], int n)
+{
+    for (int i=1; i<n; i++) {
+        if(a[i] < a[i-1]) { // means there need a re-order
+            int temp = a[i];
+            int j;
+         //find place j which a[j] <= temp, if a[j] > temp, move the element backward
+            for (j=i-1; j>=0 && a[j]>temp; j--) {
+                a[j+1]=a[j];
+            }
+        //find place j which a[j] <= temp
+        //put a[i] to place j+1
+            a[j+1]=temp;
+        }
+    }
+}
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a=*b;
+    *b=temp;
+}
+
+void insert_sort3 (int a[], int n)
+{
+    for (int i=1; i<n; i++) {
+//.        if(a[i] < a[i-1]) { // means there need a re-order, acutally no use since in below we check a[j] and a[j-1]
+         //find place j which a[j] <= temp, if a[j] > temp, just swap a[i] forward 
+        for (int j=i-1; j>=0 && a[j]>a[j+1]; j--) {
+            swap(&a[j], &a[j+1]);
+        }
+//        }
+    }
+}
+
+void check(int a[], int n)
+{
+    for(int i=1; i<n; i++) {
+        for(int j=i-1; j>=0; j--) {
+             if(a[j]>a[i])
+                 assert(0);
+        }
+    }
+}
+
 #define NUM 20 
 
 int main(void)
@@ -34,10 +82,13 @@ int main(void)
     }
     printf("\n");
 //    int a[10] = {7,6,3,0,11,11,5,9,8,1};
-    insert_sort(a,NUM);
+//    insert_sort(a,NUM);
+//    insert_sort2(a, NUM);
+    insert_sort3(a, NUM);  
     for (int i=0; i<NUM; i++)
         printf("%d ", a[i]);
-
+    printf("\n");
+    check(a,NUM);
     printf("\n");
     return 0;
 }
